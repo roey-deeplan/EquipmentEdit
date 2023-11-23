7742a6d570cc1b63b8520c6d2b48c3fb85c76fb9
amir-PZProjects <roey@deeplan.co.il>
2023-11-23 13:49:04 +0200

    init

---
+++ src/webparts/equipmentEdit/components/IEquipmentEditProps.ts
@@ -0,0 +1,7 @@
+export interface IEquipmentEditProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/equipmentEdit/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEquipmentEditWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EquipmentEditWebPartStrings' {
+  const strings: IEquipmentEditWebPartStrings;
+  export = strings;
+}
